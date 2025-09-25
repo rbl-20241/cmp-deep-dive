7359ed4ad9ed07616f819531c05115d0a37bd951
rbl-20241 <blom@casema.nl>
2025-09-25 14:36:56 +0200

    138

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-tickets',
@@ -8,5 +9,6 @@ import { NewTicketComponent } from "./new-ticket/new-ticket.component";
   styleUrl: './tickets.component.css'
 })
 export class TicketsComponent {
+  tickets: Ticket[] = [];
 
 }
