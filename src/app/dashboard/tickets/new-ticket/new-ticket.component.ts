c98394117ae4b4bb5e57487588e4877e8082310f
rbl-20241 <blom@casema.nl>
2025-09-25 16:35:16 +0200

    145

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -12,6 +12,8 @@ import { FormsModule } from '@angular/forms';
 export class NewTicketComponent implements OnInit, AfterViewInit{
   // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  enteredTitle = '';
+  enteredText = '';
   add = output<{title: string; text: string}>();
 
   ngOnInit(): void {
@@ -24,9 +26,11 @@ export class NewTicketComponent implements OnInit, AfterViewInit{
     console.log(this.form().nativeElement);
   }
 
-  onSubmit(title: string, ticketText: string,) {
-    this.add.emit({ title: title, text: ticketText});
-    this.form().nativeElement.reset();
+  onSubmit() {
+    this.add.emit({ title: this.enteredTitle, text: this.enteredText});
+    //this.form().nativeElement.reset();
+    this.enteredTitle = '';
+    this.enteredText = '';
   }
 
 }
